cc562bb907d3c19432406fd1cdb2b509569a5cfb
Ekaterina173 <51832550+Ekaterina173@users.noreply.github.com>
2021-04-18 19:44:03 +0300

    added endpoints for cinemaModel

--- api/cinema/router.js
+++ api/cinema/router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
-const Cinema = require("./model");
+const Cinemas = require("./model");
 
 router.get("/", async (req, res, next) => {
-  Cinema.find()
+  Cinemas.find()
     .exec()
     .then((cinemas) => {
       res.status(200).json(cinemas);
@@ -10,13 +10,51 @@ router.get("/", async (req, res, next) => {
     .catch(next);
 });
 
-router.get("/:id", async (req, res, next) => {
-  Cinema.findById()
+router.get("/:cinema_id", async (req, res, next) => {
+  Cinemas.findById(req.params.cinema_id)
     .exec()
     .then((cinema) => {
       res.status(200).json(cinema);
     })
     .catch(next);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const cinema = await new Cinemas(req.body).save();
+    res.status(201).json(cinema);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:cinema_id", async (req, res, next) => {
+  const bodyReducer = Object.keys(req.body).reduce((acc, curr) => {
+    if (req.body[curr]) {
+      acc[curr] = req.body[curr];
+    }
+    return acc;
+  }, {});
+  try {
+    const updatedCinema = await Cinemas.findOneAndUpdate(
+      req.params.cinema_id,
+      bodyReducer
+    ).exec();
+    res.status(200).json(updatedCinema);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:cinema_id", async (req, res, next) => {
+  try {
+    const deletedCinema = await Cinemas.findByIdAndDelete(
+      req.params.cinema_id
+    ).exec();
+    res.status(200).json(deletedCinema);
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
