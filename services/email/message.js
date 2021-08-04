a2a48987e64db01af771d508b312391de4f14299
Andt1dot <marin.cebotari94@gmail.com>
2021-08-04 21:27:10 +0300

    smallChangeProduction

--- services/email/message.js
+++ services/email/message.js
@@ -121,7 +121,7 @@ div[style*="margin: 16px 0;"] {
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
             <tr>
                 <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                    <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Bun venit la <br> Olymp Cinema !</br></h1> <img src="http://localhost:3000/static/media/olymp-cinema-logo.5cad48e1.png" width="125" height="120" style="display: block; border: 0px;" />
+                    <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Bun venit la <br> Olymp Cinema !</br></h1> <img src="https://api-olymp-cinema.herokuapp.com/static/media/olymp-cinema-logo.5cad48e1.png" width="125" height="120" style="display: block; border: 0px;" />
                 </td>
             </tr>
         </table>
@@ -142,7 +142,7 @@ div[style*="margin: 16px 0;"] {
                             <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
                                 <table border="0" cellspacing="0" cellpadding="0">
                                     <tr>
-                                        <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B"><a href="http://localhost:4000/api/auth/register-confirm/${token.token}/${user_id}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirmare profil</a></td>
+                                        <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B"><a href="https://api-olymp-cinema.herokuapp.com/api/auth/register-confirm/${token.token}/${user_id}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirmare profil</a></td>
                                     </tr>
                                 </table>
                             </td>
